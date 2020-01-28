131314c12dd6d97721df71498f85be33c9639ace
onothoja marho <marho219@gmail.com>
2020-01-28 22:14:16 +0100

    database model build for application backend

--- backend/meetups/models.py
+++ backend/meetups/models.py
@@ -1,1 +1,27 @@
 from . import db
+from datetime import datetime
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    contact = db.Column(db.Integer, unique=False)
+    email = db.Column(db.String(20), nullable=False, unique=True)
+    meetups = db.Relationship("Events", secondary="links", laszy="subquery",
+                              backref=db.backref("meetups", lazy="dynamic"))
+
+
+links = db.Table("links",
+                 db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+                 db.Column("events_id", db.Integer, db.ForeignKey("events.id"), primary_key=True)
+                 )
+
+
+class Events(db.Model):
+    id = db.Column(db.Integer, nullable=False, unique=False)
+    name = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(20), nullable=False)
+    category = db.Column(db.String(20), nullable=False)
+    date = db.Column(db.String(20), nullable=False, default=datetime.utcnow)
