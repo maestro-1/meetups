c099da652ac5f05a971ee7e15a61ee0e580826dd
onothoja marho <marho219@gmail.com>
2020-01-29 16:46:29 +0100

    About page view

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-layout row>
+      <v-flex xs12 xs6 offset-xs5>
+        <h1>About Page</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 xs6 offset-xs4>
+        <h3>Handling Your Events so You Don't have To</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 xs6 offset-xs3>
+        <p>
+          We deal with event management, automating remainders for events,
+          sending out, requesting and receiving invitations. This is a clean way
+          to set up meetings, parties, weddings and other events without ever
+          having to worry about invitations getting lost in the mail.
+        </p>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 xs6 offset-xs5>
+        <v-btn depressed routter to="/">
+          home
+        </v-btn>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
