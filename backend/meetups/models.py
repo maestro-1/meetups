d3caf8d350fe80e47d38990b06c474c603670d55
onothoja marho <marho219@gmail.com>
2020-01-30 02:40:28 +0100

    completing database model for app

--- backend/meetups/models.py
+++ backend/meetups/models.py
@@ -8,14 +8,10 @@ class Users(db.Model):
     last_name = db.Column(db.String(20), nullable=False)
     contact = db.Column(db.Integer, unique=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
+    imageUrl = db.Column(db.String(25), nullable=False, default="profile.jpg")
+    invitation = db.Relationship("Invites", backref="guest", lazy=True)
     meetups = db.Relationship("Events", secondary="links", laszy="subquery",
-                              backref=db.backref("meetups", lazy="dynamic"))
-
-
-links = db.Table("links",
-                 db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
-                 db.Column("events_id", db.Integer, db.ForeignKey("events.id"), primary_key=True)
-                 )
+                            backref=db.backref("meetups", lazy="dynamic"))
 
 
 class Events(db.Model):
@@ -24,4 +20,18 @@ class Events(db.Model):
     description = db.Column(db.String(300), nullable=False)
     location = db.Column(db.String(100), nullable=False)
     category = db.Column(db.String(20), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date = db.Column(db.DateTime, nullable=False)
+    invites = db.Relationship("Invites", backref="event", lazy=True)
+    imageUrl = db.Column(db.String(25), nullable=False, default="default.jpg")
+
+
+links = db.Table("links",
+                 db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+                 db.Column("events_id", db.Integer, db.ForeignKey("events.id"), primary_key=True)
+                 )
+
+
+class Invites(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    guest = db.Column(db.Integer, nullable=False, db.ForeignKey(users.id), uselist=False, unique=True)
+    event = db.Column(db.Integer, nullable=False, db.ForeignKey(events.id))
---
+++ backend/migrations/versions/eda627f90281_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: eda627f90281
+Revises: 
+Create Date: 2020-01-30 02:30:31.759747
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eda627f90281'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
