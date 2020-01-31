23b326039dc06bd801c22055a0eb31973e8da506
onothoja marho <marho219@gmail.com>
2020-01-31 13:04:05 +0100

    security fixes

--- backend/meetups/events/routes.py
+++ backend/meetups/events/routes.py
@@ -1,18 +1,20 @@
-from multiprocessing import Pipe
 from meetups import db
+from multiprocessing import Queue, Lock
 from meetups.models import Events, Invites
 from flask import jsonify, request, Blueprint
 from meetups.modelSchema import EventSchema, uploads
+from werkzeug.exceptions import BadRequestKeyError
 
 
 events = Blueprint("events", __name__)
-collect, sender = Pipe()
+queue = Queue()
+lock = Lock()
 
 
 @events.route("/meetups")
 def all_events():
     event_schema = EventSchema(many=True)
-    page = request.args.get("page", 1, type=int)
+    # page = request.args.get("page", 1, type=int)
     # events = Events.query.get().paginate(page=page, per_page=5)
     meetups = Events.query.all()
     meetups = event_schema.dump(meetups)
@@ -21,15 +23,21 @@ def all_events():
 
 @events.route("/meetup/<int:event_id>")
 def single_event(event_id):
-    events = Events.query.get_or_404(event_id)
-    return jsonify(events)
+    event_schema = EventSchema()
+    event = Events.query.get_or_404(event_id)
+    event = event_schema.dump(event)
+    return jsonify(event)
 
 
 @events.route("/meetup/create", methods=["POST"])
 def create_event():
     event_schema = EventSchema()
     event = event_schema.load(request.json)
-    imageUrl = collect.recv()
+
+    lock.acquire()
+    imageUrl = queue.get()
+    lock.release()
+
     if imageUrl == "Not an image":
         return jsonify("Not an Image")
     new_event = Events(title=event["title"], description=event["description"],
@@ -43,9 +51,11 @@ def create_event():
 
 @events.route("/meetup/create/file", methods=["POST"])
 def file_upload():
-    if request.files is not None:
-        file = request.files["imageUrl"]
-        file_name = uploads(file, "event_image")
-        sender.send(file_name)
-        sender.close()
+    try:
+        if request.files is not None:
+            file_name = uploads(request.files["imageUrl"], "event_image")
+            queue.put(file_name)
+            return jsonify("Image received")
+    except BadRequestKeyError:
+        queue.put("default.jpg")
         return jsonify("Image received")
