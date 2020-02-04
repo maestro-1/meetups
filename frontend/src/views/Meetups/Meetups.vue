a5f78ed2d1551c8aa64d3f2a9561b7acba52d294
onothoja marho <marho219@gmail.com>
2020-02-04 13:19:02 +0100

    state management

--- frontend/src/views/Meetups/Meetups.vue
+++ frontend/src/views/Meetups/Meetups.vue
@@ -2,17 +2,17 @@
   <v-container>
     <v-row dense class="justify-center">
       <v-col v-for="(item, i) in state.items" :key="i" cols="10">
-        <v-card :color="item.color" dark>
+        <v-card :color="item.color">
           <div class="d-flex flex-no-wrap justify-space-between">
             <div>
               <v-card-title class="headline" v-text="item.title"></v-card-title>
 
-              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+              <v-card-subtitle v-text="item.description"></v-card-subtitle>
             </div>
-
+            <!-- 
             <v-avatar class="ma-3" size="180" tile width="200">
               <v-img :src="item.src"></v-img>
-            </v-avatar>
+            </v-avatar> -->
           </div>
         </v-card>
       </v-col>
@@ -21,34 +21,46 @@
 </template>
 
 <script>
-import { reactive } from "@vue/composition-api";
+import { reactive, onMounted } from "@vue/composition-api";
 
 export default {
-  setup() {
+  setup(props, { root: { $store } }) {
     const state = reactive({
       items: [
-        {
-          color: "#1F7087",
-          src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-          title: "Supermodel",
-          artist: "Foster the People"
-        },
-        {
-          color: "#952175",
-          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-          title: "Halcyon Days",
-          artist: "Ellie Goulding"
-        },
-        {
-          color: "#953195",
-          src:
-            "https://tse1.mm.bing.net/th?id=OIP.OCUUMOVcgiJFen5ASCIpowHaFj&pid=Api&P=0&w=216&h=163",
-          title: "Memories",
-          artist: "Maroon 5"
-        }
+        // {
+        //   color: "#1F7087",
+        //   src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        //   title: "Supermodel",
+        //   artist: "Foster the People"
+        // },
+        // {
+        //   color: "#952175",
+        //   src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
+        //   title: "Halcyon Days",
+        //   artist: "Ellie Goulding"
+        // },
+        // {
+        //   color: "#953195",
+        //   src:
+        //     "https://tse1.mm.bing.net/th?id=OIP.OCUUMOVcgiJFen5ASCIpowHaFj&pid=Api&P=0&w=216&h=163",
+        //   title: "Memories",
+        //   artist: "Maroon 5"
+        // }
       ]
     });
 
+    const upComingEvents = () => {
+      let events = $store.getters.availableEvents;
+      if (events.length == 0) {
+        setTimeout(upComingEvents, 1000);
+      }
+      return state.items.push(...events);
+    };
+
+    onMounted(() => {
+      $store.dispatch("getEvents");
+      upComingEvents();
+    });
     return { state };
   }
 };
