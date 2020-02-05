4075bca9f2204f36bdc5f7cb566de90b48537984
onothoja marho <marho219@gmail.com>
2020-02-05 02:29:10 +0100

    statemanagement improvements

--- frontend/src/views/Meetups/Meetups.vue
+++ frontend/src/views/Meetups/Meetups.vue
@@ -8,11 +8,12 @@
               <v-card-title class="headline" v-text="item.title"></v-card-title>
 
               <v-card-subtitle v-text="item.description"></v-card-subtitle>
+              <v-card-subtitle v-text="item.location"></v-card-subtitle>
             </div>
-            <!-- 
-            <v-avatar class="ma-3" size="180" tile width="200">
+            
+            <v-avatar class="ma-3" size="180" tile width="180">
               <v-img :src="item.src"></v-img>
-            </v-avatar> -->
+            </v-avatar>
           </div>
         </v-card>
       </v-col>
@@ -27,32 +28,32 @@ export default {
   setup(props, { root: { $store } }) {
     const state = reactive({
       items: [
-        // {
-        //   color: "#1F7087",
-        //   src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-        //   title: "Supermodel",
-        //   artist: "Foster the People"
-        // },
-        // {
-        //   color: "#952175",
-        //   src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-        //   title: "Halcyon Days",
-        //   artist: "Ellie Goulding"
-        // },
-        // {
-        //   color: "#953195",
-        //   src:
-        //     "https://tse1.mm.bing.net/th?id=OIP.OCUUMOVcgiJFen5ASCIpowHaFj&pid=Api&P=0&w=216&h=163",
-        //   title: "Memories",
-        //   artist: "Maroon 5"
-        // }
+        {
+          color: "#1F7087",
+          src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+          title: "Supermodel",
+          artist: "Foster the People"
+        },
+        {
+          color: "#952175",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
+          title: "Halcyon Days",
+          artist: "Ellie Goulding"
+        },
+        {
+          color: "#953195",
+          src:
+            "https://tse1.mm.bing.net/th?id=OIP.OCUUMOVcgiJFen5ASCIpowHaFj&pid=Api&P=0&w=216&h=163",
+          title: "Memories",
+          artist: "Maroon 5"
+        }
       ]
     });
 
     const upComingEvents = () => {
       let events = $store.getters.availableEvents;
       if (events.length == 0) {
-        setTimeout(upComingEvents, 1000);
+          setTimeout(upComingEvents, 1000);
       }
       return state.items.push(...events);
     };
